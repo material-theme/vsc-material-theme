c271d7551339318e881423ab678d9078c14b27e3
EquinusOcio <astorino.design@gmail.com>
2017-01-06 20:09:27 +0100

    release: release 4.0.4

--- .gulp/tasks/changelog.js
+++ .gulp/tasks/changelog.js
@@ -7,7 +7,6 @@
 import gulp from 'gulp';
 import paths from '../paths';
 import yrgv from 'yargs';
-import runSequence from 'run-sequence';
 import conventionalChangelog from 'gulp-conventional-changelog';
 
 
@@ -15,7 +14,7 @@ gulp.task('changelog', () => {
   return gulp.src('CHANGELOG.md')
     .pipe(conventionalChangelog({
       preset: 'angular',
-      releaseCount: 1
+      releaseCount: 0
     }))
     .pipe(gulp.dest('./'));
 });
--- utils/info.py
+++ utils/info.py
@@ -3,7 +3,7 @@ import sublime
 import sublime_plugin
 import textwrap
 
-__version__ = '4.0.3'
+__version__ = '4.0.4'
 __pc_name__ = 'Material Theme'
 
 
