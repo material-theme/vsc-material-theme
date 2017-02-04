53f35715d23358f3268ecdb85d0bbe4fbe18b3fe
Mattia Astorino <astorino.design@gmail.com>
2017-02-04 13:01:19 +0100

    chore: Add icons process

--- .gulp/tasks/icons.js
+++ .gulp/tasks/icons.js
@@ -6,36 +6,17 @@
 
 import Gulp from 'gulp';
 import Colorize from 'gulp-colorize-svgs';
+import colors from 'colors';
 import Paths from '../paths';
+import del from 'del';
+
+import iconsColors from '../../icons/colors.js';
 
 Gulp.task('process:icons', () => {
+  del([`${Paths.icons}/svg/*.svg`]).then(paths => {
+    console.log('[ ⚙️ Deleting all icons]\n'.bold.red);
+  });
   Gulp.src(`${Paths.src}/icons/*.svg`)
-    .pipe(Colorize({
-      colors: {
-        // All files
-        default: {
-          green: '90A959',
-          blue: '6A9FB5',
-          blueDesaturated: '78909C',
-          red: 'AC4142',
-          redLight: 'D14748',
-          maroon: 'AB7558',
-          limeGreen: 'B8E15E',
-          greenTea: '2ECC71',
-          purple: 'AA759F',
-          purpleDark: '8251A8',
-          yellow: 'F4BF75',
-          teal: '80CBC4',
-          orange: 'D28445',
-        },
-        // Specific files
-        icon2: {
-          green: '00ff00'
-        }
-      },
-      replaceColor: function (content, hex) {
-        return content.replace('#000', '#' + hex);
-      }
-    }))
-    .pipe(Gulp.dest(Paths.icons));
+    .pipe(Colorize(iconsColors))
+    .pipe(Gulp.dest(`${Paths.icons}/svg`));
 });
