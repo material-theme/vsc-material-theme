5aa24248a19ce415d557b4a63f523b943145cd1a
Sundeep Malladi <sundeep.malladi@gmail.com>
2017-04-06 09:41:46 -0500

    Swapping in real template vars

--- .gulp/tasks/themes.js
+++ .gulp/tasks/themes.js
@@ -1,33 +1,35 @@
 import fs from 'fs';
 import gulp from 'gulp';
+import gutil from 'gulp-util';
 import Mustache from 'mustache';
 import YAML from 'yamljs';
+
+import Paths from '../paths';
 
 const themeCommons = require('../../src/themes/settings/commons.json');
 const themeVariants = [];
 const themeTemplateFile = fs.readFileSync(
-  './src/themes/theme-template.yml',
+  `${Paths.themes}/theme-template.yml`,
   'utf-8'
 );
 
-const files = fs.readdirSync('./src/themes/settings/specific');
+const files = fs.readdirSync(`${Paths.themes}/settings/specific`);
 
 // build theme variants for later use in templating
 files.forEach(file => {
   const name = file.split('.')[0];
-  const filepath = './src/themes/settings/specific/' + file;
+  const filepath = `${Paths.themes}/settings/specific/${file}`;
   const contents = fs.readFileSync(filepath, 'utf-8');
 
   try {
     themeVariants.push(JSON.parse(contents));
   } catch (err) {
-    console.log('Error when parsing json for theme variants', err);
+    gutil.log('Error when parsing json for theme variants', err);
   }
 });
 
 gulp.task('build:themes', cb => {
   themeVariants.forEach(variant => {
-    console.log('Building ', variant.name);
     const templateData = {
       commons: themeCommons,
       variant,
@@ -37,10 +39,14 @@ gulp.task('build:themes', cb => {
       Mustache.render(themeTemplateFile, templateData)
     );
 
+    const path = `${Paths.themes}/${variant.name}.json`;
+
     fs.writeFileSync(
-      `./src/themes/${variant.name}.json`,
+      path,
       JSON.stringify(templateJson, null, 2),
       'utf-8'
     );
+
+    gutil.log('Generated', gutil.colors.green(path));
   });
 });
