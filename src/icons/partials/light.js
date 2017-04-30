f543a31da451763fefcd4205e1e9e0ae654d56dd
Mattia Astorino <mattia.astorino@contactlab.com>
2017-04-30 22:38:33 +0200

    chore: Improve workbench theme
    Close #12, #13

--- src/icons/partials/light.js
+++ src/icons/partials/light.js
@@ -1,5 +1,18 @@
 "light": {
   "folderExpanded": "_folder_open",
   "folder": "_folder_light",
-  {{> folderNames}}
+  "folderNames": {
+    "node_modules": "_file_nodejs",
+    ".git": "_file_git",
+    ".github": "_file_github",
+    ".gulp": "_file_gulp",
+    "bower_components": "_file_bower"
+  },
+  "folderNamesExpanded": {
+    "node_modules": "_file_nodejs",
+    ".git": "_file_git",
+    ".github": "_file_github",
+    ".gulp": "_file_gulp",
+    "bower_components": "_file_bower"
+  }
 },
