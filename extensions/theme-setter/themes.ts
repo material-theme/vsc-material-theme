4329d419a8b0818bce6038728a8249ddd2175c2c
OctoD <iamoctod@gmail.com>
2017-05-29 18:01:12 +0200

    chore: dumped theme setter

--- extensions/theme-setter/themes.ts
+++
@@ -1,8 +0,0 @@
-import { IGenericObject } from "../interfaces/igeneric-object";
-
-export const THEMES: IGenericObject<string> = {
-  Default: 'Material Theme',
-  Darker: 'Material Theme Darker',
-  Lighter: 'Material Theme Lighter',
-  'Pale night': 'Material Theme Palenight'
-}
