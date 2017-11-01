681777901bd089dca46f31b42a28651b63a0072b
equinusocio <astorino.mattia@gmail.com>
2017-11-01 21:30:14 +0100

    chore: Add folder_gulp icon (wip)

--- src/icons/partials/folderNames.js
+++ src/icons/partials/folderNames.js
@@ -2,8 +2,8 @@
   "node_modules": "_file_nodejs",
   ".git": "_file_git",
   ".github": "_file_github",
-  ".gulp": "_file_gulp",
   ".vscode": "_folder_vscode",
+  ".gulp": "_folder_gulp",
   "bower_components": "_file_bower",
   "build": "_folder_dark_build",
   "dist": "_folder_dark_build"
@@ -12,7 +12,7 @@
   "node_modules": "_file_nodejs",
   ".git": "_file_git",
   ".github": "_file_github",
-  ".gulp": "_file_gulp",
+  ".gulp": "_folder_gulp_open",
   "bower_components": "_file_bower",
   ".vscode": "_folder_vscode_open",
   "build": "_folder_open_build",
