adcb3046246864eda2341b72df6f94136ec7f310
equinusocio <astorino.mattia@gmail.com>
2017-11-14 11:57:03 +0100

    fix: New bower_components folder icon

--- src/icons/partials/folderNames.js
+++ src/icons/partials/folderNames.js
@@ -1,6 +1,7 @@
 "folderNames": {
   "node_modules": "_folder_node",
   "assets": "_folder_assets",
+  "bower_components": "_folder_bower",
   "js": "_folder_js",
   "src": "_folder_src",
   "images": "_folder_images",
@@ -8,12 +9,12 @@
   ".github": "_file_github",
   ".vscode": "_folder_vscode",
   ".gulp": "_folder_gulp",
-  "bower_components": "_file_bower",
   "build": "_folder_dark_build",
   "dist": "_folder_dark_build"
 },
 "folderNamesExpanded": {
   "node_modules": "_folder_node_open",
+  "bower_components": "_folder_bower_open",
   "assets": "_folder_assets_open",
   "src": "_folder_src_open",
   "images": "_folder_images_open",
@@ -21,7 +22,6 @@
   ".git": "_file_git",
   ".github": "_file_github",
   ".gulp": "_folder_gulp_open",
-  "bower_components": "_file_bower",
   ".vscode": "_folder_vscode_open",
   "build": "_folder_open_build",
   "dist": "_folder_open_build"
