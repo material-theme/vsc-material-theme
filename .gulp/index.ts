89b06d1a76b46c336e0e19704840c54760f84217
Mattia Astorino <astorino.mattia@gmail.com>
2019-12-09 09:54:24 +0100

    Test generator

--- .gulp/index.ts
+++ .gulp/index.ts
@@ -1,8 +1,8 @@
 // export the tasks
-export * from './tasks/themes';
+// export * from './tasks/themes';
 export * from './tasks/watcher';
 export * from './tasks/changelog-title';
 export * from './tasks/copy-ui';
 
 // export default script
-export default ['build:themes'];
+export default ['build:copy-ui'];
