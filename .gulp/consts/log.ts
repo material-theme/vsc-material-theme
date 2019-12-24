1ddcebf4c0a54387d0c8dba6ec677d52018c2cfc
Alessio Occhipinti <LasaleFamine@users.noreply.github.com>
2019-12-24 11:28:38 +0100

    Feat/generator 3 iteration (#399)
    * fix: vscode tasks and removed gulp files
    
    * chore: cleanup deps and remove babel references
    
    * chore: switch from cp to ncp

--- .gulp/consts/log.ts
+++
@@ -1,5 +0,0 @@
-export const HR: string = '\n———————————————————————————————————————————————————————————————\n';
-export const MESSAGE_BUMP_ERROR: string = ' There was an issue bumping version:\n';
-export const MESSAGE_BUMP_SUCCESS: string = ' Finished successfully\n';
-export const MESSAGE_GENERATED: string = 'Generated';
-export const MESSAGE_THEME_VARIANT_PARSE_ERROR: string = 'Error when parsing json for theme variants';
--- .gulp/index.ts
+++
@@ -1,8 +0,0 @@
-// export the tasks
-// export * from './tasks/themes';
-export * from './tasks/watcher';
-export * from './tasks/changelog-title';
-export * from './tasks/copy-ui';
-
-// export default script
-export default ['build:copy-ui'];
